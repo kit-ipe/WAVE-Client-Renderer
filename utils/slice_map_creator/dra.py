dc89c9cd636c3cb31cc8aec7fcfa84f208b7e07e
Aleksandr Lizin <sogimu@nxt.ru>
2015-07-30 13:08:56 +0100

    Created base functional for slicemap_creator

--- utils/slice_map_creator/dra.py
+++ utils/slice_map_creator/dra.py
@@ -5,6 +5,8 @@ import sys
 import argparse
 import re
 
+import os.path
+
 from src.slicemap_creator import SliceMapCreator
 
 version = "0.0.0.0"
@@ -13,7 +15,7 @@ def resolution_parse(string):
     if not(bool(re.match("^([0-9]+x[0-9]+)$", string))):
         msg = "%r bad format " % string
         raise argparse.ArgumentTypeError(msg)
-    return string.split('x')
+    return [ int(i) for i in string.split('x') ]
 
 def slice_range_parse(string):
     if not(bool(re.match("^([0-9]+x[0-9,*]+)$", string))):
@@ -29,6 +31,12 @@ def area_of_slice_parse(string):
     point_left = point_strings[0].split('x')
     point_right = point_strings[1].split('x')
     return [point_left, point_right]
+
+def path_parse(string):
+    if not(bool(os.path.isdir(string))):
+        msg = "%r bad path " % string
+        raise argparse.ArgumentTypeError(msg)
+    return string
 
 def createParser ():
     # Создаем класс парсера
@@ -65,44 +73,44 @@ def createParser ():
     create_group = create_parser.add_argument_group (title='Settings')
  
     # Добавляем параметры
-    create_group.add_argument ('-psi', '--path-to-images', type=str, default='./',
+    create_group.add_argument ('-ps', '--path-to-slices', type=path_parse, required=True,
             help = 'Absolute path to images (slices).',
             metavar = 'STR')
 
-    create_group.add_argument ('-inf', '--images-name-format', type=str, default='slice-*.png',
-            help = 'Format names of images (slices).',
+    create_group.add_argument ('-snf', '--slice-name-format', type=str, default='slice-*.png',
+            help = r'Format name of image (slices). Example: slice_{d}.png',
             metavar = 'STR')
 
-    create_group.add_argument ('-psm', '--path-to-slicemaps', type=str, default='./',
-            help = 'Absolute path to images (slices).',
+    create_group.add_argument ('-psm', '--path-to-slicemaps', type=path_parse, default='./',
+            help = 'Absolute path to slicemaps images. Example: /home/user/data/slices/',
             metavar = 'STR')
 
-    create_group.add_argument ('-smnf', '--slicemaps-name-format', type=str, default='slicemap{%d}.png',
-            help = 'Format names of slicemaps.',
+    create_group.add_argument ('-smnf', '--slicemap-name-format', type=str, default='slicemap{0}.png',
+            help = r'Format name of slicemap. Example: slicemap{0}.png',
             metavar = 'STR')
 
-    create_group.add_argument ('-ss', '--slicemap-size', type=str, default='4096x4096', choices=['512x512', '1024x1024', '2048x2048', '4096x4096', '8192x8192', '16384x16384'], 
-            help = 'Size of slicemap. Format: ^([0-9]+x[0-9]+)$. Example: 4096x4096',
-            metavar = 'STR')
-
-    create_group.add_argument ('-rc', '--row-col', type=resolution_parse, default='10x10', 
-            help = 'Number of rows and cols in slicemap. Format: ^([0-9]+x[0-9]+)$',
-            metavar = 'STR')
-
-    create_group.add_argument ('-sr', '--slices-range', type=slice_range_parse, default='0x*',
-            help = 'Range of slices for slicemap. Format: ^([0-9]+x[0-9,*]+)$',
-            metavar = 'STR')
-
-    # create_group.add_argument ('-sn', '--slicemaps-number', type=int, default=1,
-    #         help = 'Number of slicemaps',
-    #         metavar = 'NUMBER')
-
-    create_group.add_argument ('-f', '--filter', type=str, default='l', choices=['n', 'l', 'bl', 'bc', 'a'],
-            help = 'Name of filter for image. n - NEAREST, bl - BILINEAR, bc - BICUBIC, a - ANTIALIAS.',
+    create_group.add_argument ('-ssm', '--slicemap-size', type=resolution_parse, default='4096x4096',
+            help = 'Size of slicemap. Possible values: "512x512", "1024x1024", "2048x2048", "4096x4096", "8192x8192", "16384x16384". Example: 4096x4096',
             metavar = 'STR')
 
     create_group.add_argument ('-as', '--area-of-slice', type=area_of_slice_parse, default='0x0,*x*',
             help = 'Area between left-top and right-bottom points on every slice for slicemap. Format: ^([0-9]+x[0-9]+,[0-9,*]+x[0-9,*]+)$. Example: 100x100,300x300',
+            metavar = 'STR')
+
+    create_group.add_argument ('-sr', '--slices-range', type=slice_range_parse, default='0x*',
+            help = 'Range of slices for slicemap. Format: ^([0-9]+x[0-9,*]+)$. Example: 100x*',
+            metavar = 'STR')
+
+    create_group.add_argument ('-rc', '--row-col', type=resolution_parse, default='10x10', 
+            help = 'Number of rows and cols in slicemap. Format: ^([0-9]+x[0-9]+)$. Example: 16x16.',
+            metavar = 'STR')
+
+    # create_group.add_argument ('-sn', '--slicemaps-number', type=int, default=1,
+    #         help = 'Number of slicemaps. Example: 3',
+    #         metavar = 'NUMBER')
+
+    create_group.add_argument ('-f', '--filter', type=str, default='bl', choices=['n', 'l', 'bl', 'bc', 'a'],
+            help = 'Name of filter for image. Possible values: "n" - NEAREST, "bl" - BILINEAR, "bc" - BICUBIC, "a" - ANTIALIAS. Example: n',
             metavar = 'STR')
 
     create_group.add_argument ('--help', '-h', action='help', help='Help')
@@ -119,12 +127,14 @@ if __name__ == '__main__':
     parser = createParser()
     namespace = parser.parse_args(sys.argv[1:])
  
-    print (namespace)
+    # print (namespace)
 
     smc = SliceMapCreator(namespace)
  
     if namespace.command == "create":
         smc.create()
+        print("Meta information: ")
+        print(smc.getJsonConfig())
     else:
         parser.print_help()
         
--- utils/slice_map_creator/src/slicemap_creator.py
+++ utils/slice_map_creator/src/slicemap_creator.py
@@ -1,21 +1,181 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+import json
+import os
+import re
+
 class SliceMapCreator:
     def create(self):
         print self.config
 
-    def __init__(self, config):
-        # self.area_of_slice = config.area_of_slice
-        # self.command = config.command
-        # self.filter = config.filter
-        # self.path_to_images = config.path_to_images
-        # self.images_name_format = config.images_name_format
-        # self.path_to_slicemaps = config.path_to_slicemaps
-        # self.slicemaps_name_format = config.slicemaps_name_format
-        # self.row_col = config.row_col
-        # self.slicemap_size = config.slicemap_size
-        # self.slicemaps_number = config.slicemaps_number
-        # self.slices_range = config.slices_range
+        import numpy
+        import StringIO
+        from PIL import Image
+        from scipy import misc
+        import math
 
+        # width = 256
+        # height = 256
+        # depth = 256
+
+        # slicesX = self.config.row_col[0]
+        # slicesY = self.config.row_col[1]
+
+        # Type of image filter for slicemap
+        if self.config.filter == 'n':
+            filter = Image.NEAREST;
+        elif self.config.filter == 'bl':
+            filter = Image.BILINEAR;
+        elif self.config.filter == 'bc':
+            filter = Image.BICUBIC;
+        elif self.config.filter == 'a':
+            filter = Image.ANTIALIAS;
+        
+        # Path to slices
+        path_to_slices = self.config.path_to_slices
+
+        # Regex exp for name of slice
+        slice_name_format = self.config.slice_name_format
+        
+        # Path for slicemaps
+        path_to_slicemaps = self.config.path_to_slicemaps
+
+        # Regex exp for name of slice
+        slicemap_name_format = self.config.slicemap_name_format
+
+        # Number of cols and rows
+        row_col = self.config.row_col
+
+        # Slicemap size
+        slicemap_size = self.config.slicemap_size
+
+        print("Slicemap size: {0}, {1}".format(slicemap_size[0], slicemap_size[1]))
+
+        # Getting list of slices in the dir
+        files_list = os.listdir(path_to_slices)
+        
+        def sort_slices(a, b):
+            a_str_val = re.findall("[0-9]+", a[::-1])[0][::-1]
+            tmp = "0";
+            copy_allowed = False
+            for i in range(0, len(a_str_val)):
+                if a_str_val[i] != '0' or copy_allowed == True:
+                    tmp += a_str_val[i]
+                    copy_allowed = True
+
+            a_str_val = tmp
+            a_value = int(a_str_val)
+
+            b_str_val = re.findall("[0-9]+", b[::-1])[0][::-1]
+            tmp = "0";
+            copy_allowed = False
+            for i in range(0, len(b_str_val)):
+                if b_str_val[i] != '0' or copy_allowed == True:
+                    tmp += b_str_val[i]
+                    copy_allowed = True
+            b_str_val = tmp                            
+            b_value = int(b_str_val)
+
+            return a_value - b_value;
+
+        files_list = sorted(files_list, cmp=sort_slices)
+
+        # print(files_list)
+
+        slices_path_list = []
+        slices_name_list = []
+        for slice_name in files_list:
+            path_to_slice = os.path.join(path_to_slices, slice_name)
+            if re.match(slice_name_format, slice_name) and os.path.isfile(path_to_slice):
+                slices_path_list.append(path_to_slice)
+                slices_name_list.append(slice_name)
+
+        # def sort_slices(a, b):
+        #     a_value = int(re.findall("[0-9]+", a)[0])
+        #     b_value = int(re.findall("[0-9]+", b)[0])
+        #     return a_value - b_value;
+
+        # slices_name_list = sorted(slices_name_list, cmp=sort_slices)
+        # print(slices_name_list)
+
+        # Slices range
+        slices_range = self.config.slices_range
+        slices_range[0] = int(self.config.slices_range[0])
+        slices_range[1] = len(slices_path_list) if self.config.slices_range[1] == "*" else int(self.config.slices_range[1])
+
+        # Number of slices
+        number_of_slices = slices_range[1] - slices_range[0]
+        print("Number of slices: {0}".format(number_of_slices))
+
+        # Original slice size
+        slice0 = Image.open(slices_path_list[0])
+        original_slice_size = [slice0.size[0], slice0.size[1]]
+        print("Original slice size: {0},{1}".format(original_slice_size[0], original_slice_size[1]))        
+
+        # Slicemap slice size
+        slicemap_slice_size = [int(math.floor(slicemap_size[0] / row_col[0])), int(math.floor(slicemap_size[1] / row_col[1]))]
+        print("Slicemap slice size: {0},{1}".format(slicemap_slice_size[0], slicemap_slice_size[1]))
+
+        # Area of every slice for slice map
+        area_of_slice = self.config.area_of_slice
+        area_of_slice[1][0] = slicemap_slice_size[0] if area_of_slice[1][0] == "*" else int(area_of_slice[1][0])
+        area_of_slice[1][1] = slicemap_slice_size[1] if area_of_slice[1][1] == "*" else int(area_of_slice[1][1])
+
+        # Images for slicemaps
+        slicemaps_images = []
+
+        slicemap_slices_number = float(row_col[0] * row_col[1])
+
+        slicemaps_number = int(math.ceil(number_of_slices / slicemap_slices_number))
+
+        for i in range(0, slicemaps_number):
+            slicemap_image = Image.new('L', (slicemap_size[0], slicemap_size[1]))
+            slicemaps_images.append(slicemap_image)
+
+        print("Number of slicemaps: {0}".format(slicemaps_number))
+
+        # Read files 1 by 1 from dir
+        for slice_id in range(slices_range[0], slices_range[1]):
+            slice_path = slices_path_list[slice_id]
+            print("Slice path: {0}".format(slice_path))
+
+            slice_global_order_id = int(slice_id - slices_range[0])
+            print("Slice global order id: {0}".format(slice_global_order_id))
+
+            slice_local_order_id = slice_global_order_id % slicemap_slices_number
+            print("Slice local order id: {0}".format(slice_local_order_id))
+            
+            slicemap_id = int(slice_global_order_id / slicemap_slices_number)
+            print("Slicemap id: {0}".format(slicemap_id))
+            
+            slice_image = Image.open(slice_path)
+            slice_image = slice_image.resize((slicemap_slice_size[0], slicemap_slice_size[1]), filter)
+
+            slice_col_pos = int(slice_local_order_id % row_col[0])
+            slice_row_pos = int(slice_local_order_id / row_col[1])
+
+            point0 = [slice_col_pos * slicemap_slice_size[0], slice_row_pos * slicemap_slice_size[1]]
+            point1 = [point0[0] + slice_image.size[0], point0[1] + slice_image.size[1]]
+
+            print(point0[0], point0[1], point1[0], point1[1])
+
+            slicemaps_images[slicemap_id].paste(slice_image, (point0[0], point0[1], point1[0], point1[1]))
+
+
+        for i in range(0, slicemaps_number):
+            slicemaps_images[i].save(os.path.join(path_to_slicemaps, slicemap_name_format.format(i)))
+
+    def getJsonConfig(self):
+        data = {}
+        data["area_of_slice"] = self.config.area_of_slice
+        data["filter"] = self.config.filter
+        data["path_to_slicemaps"] = self.config.path_to_slicemaps
+        data["path_to_slices"] = self.config.path_to_slices
+        data["row_col"] = self.config.row_col
+        data["slicemap_size"] = self.config.slicemap_size
+        data["slices_range"] = self.config.slices_range
+        return json.dumps(data)
+
+    def __init__(self, config):
         self.config = config
