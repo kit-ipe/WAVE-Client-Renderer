39c2cf2c5006ec56afdc21c72b324d1c1263f694
Raghav Arora <agu94.raghav@gmail.com>
2018-03-16 12:14:13 +0100

    Added CORS Support

--- src/core.js
+++ src/core.js
@@ -436,72 +436,74 @@ Core.prototype._secondPassSetUniformValue = function(key, value) {
 };
 
 
-Core.prototype._setSlicemapsTextures = function(images) {
-    var textures = [];
-
-    for(var i=0; i<images.length; i++) {
-        var texture = new THREE.Texture( images[i] );
-        texture.magFilter = THREE.LinearFilter;
-        texture.minFilter = THREE.LinearFilter;
-        texture.wrapS = texture.wrapT = THREE.ClampToEdgeWrapping;
-        texture.generateMipmaps = false;
-        texture.flipY = false;
-        texture.needsUpdate = true;
-
-        textures.push(texture);
-    };
-    this._slicemaps_textures = textures;
-};
-
-
-Core.prototype.setTransferFunctionByImage = function(image) {
-    this._transfer_function_as_image = image;
-    var texture = new THREE.Texture(image);
-    texture.magFilter = THREE.LinearFilter;
-    texture.minFilter = THREE.LinearFilter;
-    texture.wrapS = texture.wrapT =  THREE.ClampToEdgeWrapping;
-    texture.generateMipmaps = false;
-    texture.flipY = true;
-    texture.needsUpdate = true;
-
-    if(this._mode == "3d") {
-        this._secondPassSetUniformValue("uTransferFunction", texture);
-    }
-    this.onChangeTransferFunction.call(image);
-};
+	Core.prototype._setSlicemapsTextures = function(images) {
+	    var textures = [];
+            var loader = new THREE.TextureLoader();
+            loader.crossOrigin = ''; 
+            for(var i=0; i<images.length; i++) {
+                this_img = images[i];
+                loader.load(this_img.src, function (texture) {
+                    texture.magFilter = THREE.LinearFilter;
+		            texture.minFilter = THREE.LinearFilter;
+                    texture.wrapS = texture.wrapT = THREE.ClampToEdgeWrapping;
+		            texture.generateMipmaps = false;
+                    texture.flipY = false;
+		            texture.needsUpdate = true;
+                    textures.push(texture);
+	        }, function (err) { console.log("error");} );
+            }
+	    this._slicemaps_textures = textures;
+	};
 
 
-Core.prototype.setTransferFunctionByColors = function(colors) {
-    this._transfer_function_colors = colors;
+	Core.prototype.setTransferFunctionByImage = function(image) {
+	    this._transfer_function_as_image = image;
+	    var texture = new THREE.Texture(image);
+	    texture.magFilter = THREE.LinearFilter;
+	    texture.minFilter = THREE.LinearFilter;
+	    texture.wrapS = texture.wrapT =  THREE.ClampToEdgeWrapping;
+	    texture.generateMipmaps = false;
+	    texture.flipY = true;
+	    texture.needsUpdate = true;
 
-    var canvas = document.createElement('canvas');
-    canvas.width  = 512;
-    canvas.height = 2;
-
-    var ctx = canvas.getContext('2d');
-
-    var grd = ctx.createLinearGradient(0, 0, canvas.width -1 , canvas.height - 1);
-
-    for(var i=0; i<colors.length; i++) {
-        grd.addColorStop(colors[i].pos, colors[i].color);
-    }
-
-    ctx.fillStyle = grd;
-    ctx.fillRect(0,0,canvas.width ,canvas.height);
-    var image = new Image();
-    image.src = canvas.toDataURL();
-    image.style.width = 20 + " px";
-    image.style.height = 512 + " px";
-
-    var transferTexture = this.setTransferFunctionByImage(image);
-
-    this.onChangeTransferFunction.call(image);
-};
+	    if(this._mode == "3d") {
+		this._secondPassSetUniformValue("uTransferFunction", texture);
+	    }
+	    this.onChangeTransferFunction.call(image);
+	};
 
 
-Core.prototype.getTransferFunctionAsImage = function() {
-    return this._transfer_function_as_image;
-};
+	Core.prototype.setTransferFunctionByColors = function(colors) {
+	    this._transfer_function_colors = colors;
+
+	    var canvas = document.createElement('canvas');
+	    canvas.width  = 512;
+	    canvas.height = 2;
+
+	    var ctx = canvas.getContext('2d');
+
+	    var grd = ctx.createLinearGradient(0, 0, canvas.width -1 , canvas.height - 1);
+
+	    for(var i=0; i<colors.length; i++) {
+		grd.addColorStop(colors[i].pos, colors[i].color);
+	    }
+
+	    ctx.fillStyle = grd;
+	    ctx.fillRect(0,0,canvas.width ,canvas.height);
+	    var image = new Image();
+	    image.src = canvas.toDataURL();
+	    image.style.width = 20 + " px";
+	    image.style.height = 512 + " px";
+
+	    var transferTexture = this.setTransferFunctionByImage(image);
+
+	    this.onChangeTransferFunction.call(image);
+	};
+
+
+	Core.prototype.getTransferFunctionAsImage = function() {
+	    return this._transfer_function_as_image;
+	};
 
 
 Core.prototype.getBase64 = function() {
